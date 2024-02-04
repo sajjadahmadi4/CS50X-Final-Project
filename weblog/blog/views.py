729c1fec5b422b16fefa1db43e175bd0f154c9b4
sajjad <sajjadahmadi497@gmail.com>
2024-02-04 02:43:40 -0800

    fixed the showing of draft articles

--- weblog/blog/views.py
+++ weblog/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 
@@ -12,8 +12,9 @@ def index(request):
         context
     )
 
+
 def detail(request, slug):
     context = {
-        "article": Article.objects.get(slug=slug)
+        "article": get_object_or_404(Article, slug=slug, status='p')
     }
     return render(request, "blog/detail.html", context)
