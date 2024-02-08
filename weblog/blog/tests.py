52127bd1b7575e6a1e4159eb733a5f63604fb2ab
sajjad <sajjadahmadi497@gmail.com>
2024-02-08 03:37:18 -0800

    added some tests for blog application

--- weblog/blog/tests.py
+++ weblog/blog/tests.py
@@ -1,3 +1,30 @@
 from django.test import TestCase
+from django.urls import resolve
 
-# Create your tests here.
+from .views import index
+
+
+class BlogTests(TestCase):
+    def test_index_page_exists(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_page_1_exists(self):
+        response = self.client.get("/page/1")
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_page_template_used(self):
+        response = self.client.get("/")
+        self.assertTemplateUsed(response, "blog/index.html")
+
+    def test_index_page_contains_correct_html(self):
+        response = self.client.get("/")
+        self.assertContains(response, "Writing Blogs Made Easy")
+
+    def test_index_page_does_contain_incorrect_html(self):
+        response = self.client.get("/")
+        self.assertNotContains(response, "I should not be on the page")
+
+    def test_index_page_url_resolve_index_view(self):
+        view = resolve("/")
+        self.assertEqual(view.func.__name__, index.__name__)
